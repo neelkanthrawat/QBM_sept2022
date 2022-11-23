e2cddce9ace71c49969339da239036dd51e46337
pafloxy <61856561+pafloxy@users.noreply.github.com>
2022-11-23 17:43:41 +0530

    edit

--- qmcmc_codes/qbm_utils_2.py
+++ qmcmc_codes/qbm_utils_2.py
@@ -188,7 +188,7 @@ def measure_and_plot(
 #   return qc_in
 
 
-@jitclass
+# @jitclass
 class IsingEnergyFunction:
     """A class to build the Ising Hamiltonian from data"""
 
@@ -204,6 +204,7 @@ class IsingEnergyFunction:
     def get_h(self):
         return self.h
 
+    @jit(nopython= True)
     def get_energy(self, state: Union[str, np.array]) -> float:
         """'state' should be a bipolar state if it is an array"""
 
@@ -218,12 +219,13 @@ class IsingEnergyFunction:
             return 0.5 * np.dot(state.transpose(), self.J.dot(state)) + np.dot(
                 self.h.transpose(), state
             )
-
+    @jit(nopython= True)
     def get_partition_sum(self, beta: float = 1.0):  ## is computationally expensive
 
         all_configs = np.array(list(itertools.product([1, 0], repeat=self.num_spins)))
         return sum([self.get_boltzmann_prob(configbeta=beta) for config in all_configs])
 
+    @jit(nopython= True)
     def get_boltzmann_prob(
         self, state: Union[str, np.array], beta: float = 1.0, normalised=False
     ) -> float:
